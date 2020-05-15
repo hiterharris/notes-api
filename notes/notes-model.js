06739d55be4e29a0d7b61c418887c793a1cf1509
Hiter Harris <hiterharris@comcast.net>
2020-05-14 20:03:59 -0400

    added delete route

--- notes/notes-model.js
+++ notes/notes-model.js
@@ -3,30 +3,19 @@ const db = require('../data/dbConfig.js');
 module.exports = {
   add,
   find,
-  findBy,
-  findById,
+  remove
 };
 
 function find() {
   return db('notes').select('id', 'title', 'date', 'text');
 }
 
-function findBy(filter) {
-  return db('notes').where(filter);
-}
-
-// function add(note) {
-//   const [id] = await db('notes').insert(note);
-
-//   return find();
-// }
-
 function add(note) {
   return db('notes').insert(note)
 }
 
-function findById(id) {
-  return db('users')
-    .where({ id })
-    .first();
+function remove(id) {
+  return db('notes')
+  .where({id})
+  .del()
 }
--- notes/notes-router.js
+++ notes/notes-router.js
@@ -24,4 +24,20 @@ router.post('/', (req, res) => {
   });
 });
 
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  Notes.remove(id)
+  .then(deleted => {
+    if (deleted) {
+      res.json({ removed: deleted });
+    } else {
+      res.status(404).json({ message: 'Could not find note with given id' });
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to delete note' });
+  });
+});
+
 module.exports = router;
